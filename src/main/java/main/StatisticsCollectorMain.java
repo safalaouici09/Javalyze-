f067bdbe9e28a1e27ed07e5456f15e5cbf392fe1
safalaouici09 <is_laouici@esi.dz>
2024-10-03 23:13:08 +0200

    fix input code

--- src/main/java/main/StatisticsCollectorMain.java
+++ src/main/java/main/StatisticsCollectorMain.java
@@ -5,11 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import collectors.ProjectAnalyser;
+import graphs.CallGraph;
+import graphs.StaticCallGraph;
 import processors.ASTProcessor;
 
 public class StatisticsCollectorMain extends AbstractMain {
     
     private static final String QUIT = "3"; // Assuming QUIT is defined as "3"
+    private ASTProcessor processor; // Declare an instance variable for ASTProcessor
 
     @Override
     protected void menu() {
@@ -27,17 +30,22 @@ public class StatisticsCollectorMain extends AbstractMain {
 
         try {
             inputReader = new BufferedReader(new InputStreamReader(System.in));
-            if (args.length < 1)
+            if (args.length < 1) {
                 setTestProjectPath(inputReader);
-            else
+            } else {
                 verifyTestProjectPath(inputReader, args[0]);
+            }
 
             String userInput = "";
+            String projectPath = TEST_PROJECT_PATH; // Assuming TEST_PROJECT_PATH is correctly set
+
             
+            //main.processor = new ASTProcessor(projectPath); 
+
             do {    
                 main.menu();            
                 userInput = inputReader.readLine();
-                main.processUserInput(userInput);
+                main.processUserInput(userInput); // Call processUserInput with userInput only
                 Thread.sleep(3000);
                 
             } while (!userInput.equals(QUIT));
@@ -50,24 +58,31 @@ public class StatisticsCollectorMain extends AbstractMain {
     }
     
     protected void processUserInput(String userInput) {
+        CallGraph callGraph = (CallGraph) processor; // Use the instance variable
+
         switch (userInput) {
-		    case "1":
-		        ProjectAnalyser analyzer = new ProjectAnalyser(TEST_PROJECT_PATH);
-		        analyzer.analyzeProject(); // Collect statistics without calling CallGraph
-		        break;
 
-		    case "2":
-		        // If you need to implement displaying a call graph, handle that here
-		        System.out.println("Call graph display functionality is not implemented.");
-		        break;
+            case "1":
+                ProjectAnalyser analyzer = new ProjectAnalyser(TEST_PROJECT_PATH);
+                analyzer.analyzeProject(); // Collect statistics without calling CallGraph
+                break;
 
-		    case QUIT:
-		        System.out.println("Bye...");
-		        return;
-		        
-		    default:
-		        System.err.println("Sorry, wrong input. Please try again.");
-		        return;
-		}
+            case "2":
+                try {
+                    callGraph = StaticCallGraph.createCallGraph(TEST_PROJECT_PATH);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                callGraph.log();
+                break;
+
+            case QUIT:
+                System.out.println("Bye...");
+                return;
+                
+            default:
+                System.err.println("Sorry, wrong input. Please try again.");
+                return;
+        }
     }
 }
