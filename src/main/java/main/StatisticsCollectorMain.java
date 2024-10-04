8e49a8ddd1fe2a1c1b295f9a69d6e96676ccccd3
safalaouici09 <is_laouici@esi.dz>
2024-10-04 10:20:54 +0200

    better management

--- src/main/java/main/StatisticsCollectorMain.java
+++ src/main/java/main/StatisticsCollectorMain.java
@@ -1,8 +1,11 @@
 package main;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import collectors.ProjectAnalyser;
 import graphs.CallGraph;
@@ -10,64 +13,89 @@ import graphs.StaticCallGraph;
 import processors.ASTProcessor;
 
 public class StatisticsCollectorMain extends AbstractMain {
-    
-    private static final String QUIT = "3"; // Assuming QUIT is defined as "3"
-    private ASTProcessor processor; // Declare an instance variable for ASTProcessor
+	  private ASTProcessor processor; // Declare an instance variable for ASTProcessor
 
+
+    private static final String QUIT = "3"; // Quit option defined as "3"
+    private static final String PROJECTS_DIRECTORY = "/home/safa/Bureau/Tp1_partie2/projectsJava"; // Define the path to your projects directory
+    
     @Override
     protected void menu() {
         StringBuilder builder = new StringBuilder();
         builder.append("1. Collect Statistics.");
         builder.append("\n2. Display Call Graph.");
         builder.append("\n" + QUIT + ". To quit.");
-        
         System.out.println(builder);
     }
 
-    public static void main(String[] args) {    
+    public static void main(String[] args) {
         StatisticsCollectorMain main = new StatisticsCollectorMain();
         BufferedReader inputReader;
 
         try {
             inputReader = new BufferedReader(new InputStreamReader(System.in));
-            if (args.length < 1) {
-                setTestProjectPath(inputReader);
-            } else {
-                verifyTestProjectPath(inputReader, args[0]);
+
+            // Check if the projects directory exists
+            File projectDir = new File(PROJECTS_DIRECTORY);
+            if (!projectDir.exists() || !projectDir.isDirectory()) {
+                System.err.println("Error: The projects directory " + PROJECTS_DIRECTORY + " doesn't exist or isn't a valid folder.");
+                return;
             }
 
+            // Ask user for project name
+            System.out.println("Available projects:");
+            listProjectsInDirectory(projectDir); // List available projects
+            System.out.println("Please enter the name of the project you want to analyze:");
+
+            String projectName = inputReader.readLine();
+            File selectedProject = new File(PROJECTS_DIRECTORY, projectName + "/src");
+
+            // Verify if selected project exists
+            while (!selectedProject.exists() || !selectedProject.isDirectory()) {
+                System.err.println("Error: The project src directory " + selectedProject.getPath() + " doesn't exist. Please try again:");
+                projectName = inputReader.readLine();
+                selectedProject = new File(PROJECTS_DIRECTORY, projectName + "/src");
+            }
+
+            TEST_PROJECT_PATH = selectedProject.getCanonicalPath(); // Set the valid project path
+
             String userInput = "";
-            String projectPath = TEST_PROJECT_PATH; // Assuming TEST_PROJECT_PATH is correctly set
-
-            
-            //main.processor = new ASTProcessor(projectPath); 
-
-            do {    
-                main.menu();            
+            do {
+                main.menu(); // Show menu
                 userInput = inputReader.readLine();
-                main.processUserInput(userInput); // Call processUserInput with userInput only
+                main.processUserInput(userInput); // Process input
                 Thread.sleep(3000);
-                
             } while (!userInput.equals(QUIT));
-            
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
+
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
-    
+
+    protected static void listProjectsInDirectory(File projectsDir) {
+        File[] projects = projectsDir.listFiles(File::isDirectory);
+        if (projects != null) {
+            for (File project : projects) {
+                System.out.println("- " + project.getName());
+            }
+        } else {
+            System.err.println("Error: Could not list projects in " + projectsDir.getPath());
+        }
+    }
+
+    @Override
     protected void processUserInput(String userInput) {
-        CallGraph callGraph = (CallGraph) processor; // Use the instance variable
-
+            CallGraph callGraph = (CallGraph) processor; 
+        // This method should be properly implemented according to your needs
         switch (userInput) {
-
             case "1":
-                ProjectAnalyser analyzer = new ProjectAnalyser(TEST_PROJECT_PATH);
-                analyzer.analyzeProject(); // Collect statistics without calling CallGraph
+                System.out.println("Collecting statistics for project at " + TEST_PROJECT_PATH);
+               ProjectAnalyser analyzer = new ProjectAnalyser(TEST_PROJECT_PATH);
+                analyzer.analyzeProject(); 
                 break;
 
             case "2":
+                System.out.println("Displaying Call Graph for project at " + TEST_PROJECT_PATH);
                 try {
                     callGraph = StaticCallGraph.createCallGraph(TEST_PROJECT_PATH);
                 } catch (IOException e) {
@@ -78,11 +106,11 @@ public class StatisticsCollectorMain extends AbstractMain {
 
             case QUIT:
                 System.out.println("Bye...");
-                return;
-                
+                break;
+
             default:
                 System.err.println("Sorry, wrong input. Please try again.");
-                return;
+                break;
         }
     }
 }
